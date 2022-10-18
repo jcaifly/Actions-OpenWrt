3bcf2cb10fc7df9ad7dc87b9a6f998c00aacd9bf
jcaifly <38722118+jcaifly@users.noreply.github.com>
2022-10-18 08:35:50 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,6 +14,6 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-git clone https://github.com/vernesong/OpenClash package/OpenClash
+git clone https://github.com/vernesong/OpenClash.git package/OpenClash
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
