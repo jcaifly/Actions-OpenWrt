14775f419df30ccb6d1227b39dfdcddb527bd889
jcaifly <38722118+jcaifly@users.noreply.github.com>
2024-08-26 23:27:34 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,5 +10,25 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
-# Modify default IP
-sed -i 's/192.168.1.1/192.168.123.1/g' package/base-files/files/bin/config_generate
+# 修改管理页面的 IP 地址
+uci set network.lan.ipaddr='192.168.123.1'
+uci commit network
+
+# 自定义管理页面的密码 (请将 'your_password' 替换为你想要设置的密码)
+NEW_PASSWORD='CAIjie@520.com'
+echo -e "$NEW_PASSWORD\n$NEW_PASSWORD" | passwd root
+
+# 自定义 2.4GHz WiFi 的 SSID 和密码 (请根据需要替换 'YourSSID_2G' 和 'YourWiFiPassword_2G')
+uci set wireless.@wifi-iface[0].ssid='OpenWrt_2.4G'
+uci set wireless.@wifi-iface[0].key='CAIjie@520.com'
+
+# 自定义 5GHz WiFi 的 SSID 和密码 (请根据需要替换 'YourSSID_5G' 和 'YourWiFiPassword_5G')
+uci set wireless.@wifi-iface[1].ssid='OpenWrt_5G'
+uci set wireless.@wifi-iface[1].key='CAIjie@520.com'
+
+# 提交无线配置
+uci commit wireless
+
+# 重新加载网络和无线配置
+/etc/init.d/network reload
+/etc/init.d/wireless reload
